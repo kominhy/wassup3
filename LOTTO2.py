30f33e750455f0b6e46a58a9fd2a01f7cd5bc18a
kom <rhalsgur357@naver.com>
2024-07-01 02:10:00 +0900

    lost

---
+++ LOTTO2.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 로또 번호 생성기
+import time
+t = input('타이머 입력:')
+for i in range(int(t)) :
+    print(i)
+    time.sleep(1)
+
+
+
+import random
+
+lotto = []
+num = []
+nums = range(1,46)
+
+for i in range(5) :
+    lotto = random.sample(nums, 6)
+    lotto.sort()
+    list = str(lotto)
+    print(i+1,'번째줄 : ', list[1:-1]) # 괄호 제거
+    
+    lotto = [] #초기화
+
+
+# In[29]:
+
+
+# import random
+
+# lotto = []
+
+# nums = range(1,46)
+# num = []
+
+# for i in range(5) :
+#     lotto = random.sample(nums, 6)
+#     lotto.sort()
+#     list = str(lotto)
+#     print(i+1,'번째줄 : ', list[1:-1]) # 괄호 제거
+    
+#     lotto = [] #초기화
+
+
+# In[28]:
+
+
+# import random
+# lotto = []
+# nums = range(1,46)
+# num = []
+# lotto = random.sample(nums, 6)
+# lotto.sort()
+# lotto
+
+
+# In[27]:
+
+
+# lotto.sort?
+
+
+# In[ ]:
+
+
+
+
